f35b8e1d924d8d52ebef31a0a4f48433a3209feb
j-miszczyszyn <kuba.miszczyszyn@gmail.com>
2024-02-08 09:27:27 +0100

    describtion of data

---
+++ R/raster_sample.R
@@ -0,0 +1,13 @@
+#' Example data
+#'
+#' Example data created manually for the purpose of this package.
+#'
+#' @format ## `raster_sample`
+#' A data frame with 10 observations on 2 variables.
+#' \describe{
+#' \item{x}{numeric vector}
+#' \item{y}{numeric vector}
+#' }
+#' @source Manually created.
+"raster_sample"
+
