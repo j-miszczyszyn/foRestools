6efae6830c76deaa078a861f16e360752cf1480b
Jakub Miszczyszyn <kuba.miszczyszyn@gmail.com>
2024-02-18 20:56:23 +0100

    issue repair

---
+++ R/ortho_download_big_area_rgugik.R
@@ -0,0 +1,23 @@
+
+#' Title
+#'
+#' @param x - path to your shapefile
+#' @param y - integer of length 1 or 2, number of grid cells in x and y direction (columns, rows)
+#'
+#' @return
+#' @export
+#'
+#' @examples
+ortho_download_big_area_rgugik=function(x,y){
+  aoi=sf::st_read(x)
+  grid=sf::st_make_grid(aoi, n=y)
+  aoi=sf::st_intersection(aoi,grid)
+  plot(aoi)
+  result=data_frame()
+  for (i in 1:length(aoi$geometry)) {
+    req_df=rgugik::ortho_request(aoi[i,])
+    result=dplyr::bind_rows(req_df, result)
+  }
+  return(result)
+  print("This tools is based on https://github.com/kadyb/rgugik")
+}
