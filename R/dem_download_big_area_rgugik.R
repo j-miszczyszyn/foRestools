9ff8f1975f9cac6c09d96f8bbabc67725dcb5d90
Jakub Miszczyszyn <kuba.miszczyszyn@gmail.com>
2024-02-18 22:39:36 +0100

    error repair

--- R/dem_download_big_area_rgugik.R
+++ R/dem_download_big_area_rgugik.R
@@ -1,9 +1,18 @@
+#' Title
+#'
+#' @param x
+#' @param y
+#'
+#' @return
+#' @export
+#'
+#' @examples
 dem_download_big_area_rgugik <- function(x, y) {
   aoi <- sf::st_read(x)
   grid <- sf::st_make_grid(aoi, n = y)
   aoi <- sf::st_intersection(aoi, grid)
   plot(aoi)
-  result <- tibble::tibble()
+  result <- data.frame()
   for (i in seq_len(nrow(aoi))) {
     req_df <- rgugik::DEM_request(aoi[i, ])
     result <- dplyr::bind_rows(result, req_df)
