f0cef352ad0ee4084c01237e740e7962ab890230
Jakub Miszczyszyn <kuba.miszczyszyn@gmail.com>
2024-02-18 22:16:55 +0100

    self repair in dem_download_big_area_rgugik

--- R/dem_download_big_area_rgugik.R
+++ R/dem_download_big_area_rgugik.R
@@ -1,23 +1,13 @@
-
-#' Title
-#'
-#' @param x - path to your shapefile
-#' @param y - integer of length 1 or 2, number of grid cells in x and y direction (columns, rows)
-#'
-#' @return
-#' @export
-#'
-#' @examples
-dem_download_big_area_rgugik=function(x,y){
-  aoi=sf::st_read(x)
-  grid=sf::st_make_grid(aoi, n=y)
-  aoi=sf::st_intersection(aoi,grid)
+dem_download_big_area_rgugik <- function(x, y) {
+  aoi <- sf::st_read(x)
+  grid <- sf::st_make_grid(aoi, n = y)
+  aoi <- sf::st_intersection(aoi, grid)
   plot(aoi)
-  result=data_frame()
-  for (i in 1:length(aoi$geometry)) {
-    req_df=rgugik::DEM_request(aoi[i,])
-    result=dplyr::bind_rows(req_df, result)
+  result <- tibble::tibble()
+  for (i in seq_len(nrow(aoi))) {
+    req_df <- rgugik::DEM_request(aoi[i, ])
+    result <- dplyr::bind_rows(result, req_df)
   }
-  return(result)
   print("This tools is based on https://github.com/kadyb/rgugik")
+  return(result)
 }
--- R/ortho_download_big_area_rgugik.R
+++ R/ortho_download_big_area_rgugik.R
@@ -1,23 +1,22 @@
-
 #' Title
 #'
-#' @param x - path to your shapefile
-#' @param y - integer of length 1 or 2, number of grid cells in x and y direction (columns, rows)
+#' @param x
+#' @param y
 #'
 #' @return
 #' @export
 #'
 #' @examples
-ortho_download_big_area_rgugik=function(x,y){
-  aoi=sf::st_read(x)
-  grid=sf::st_make_grid(aoi, n=y)
-  aoi=sf::st_intersection(aoi,grid)
+ortho_download_big_area_rgugik <- function(x, y) {
+  aoi <- sf::st_read(x)
+  grid <- sf::st_make_grid(aoi, n = y)
+  aoi <- sf::st_intersection(aoi, grid)
   plot(aoi)
-  result=data_frame()
-  for (i in 1:length(aoi$geometry)) {
-    req_df=rgugik::ortho_request(aoi[i,])
-    result=dplyr::bind_rows(req_df, result)
+  result <- tibble::tibble()
+  for (i in seq_len(nrow(aoi))) {
+    req_df <- rgugik::ortho_request(aoi[i, ])
+    result <- dplyr::bind_rows(result, req_df)
   }
-  return(result)
   print("This tools is based on https://github.com/kadyb/rgugik")
+  return(result)
 }
